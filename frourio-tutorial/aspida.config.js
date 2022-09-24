328b8d105dd5ea22390746fe3d4509f3bf44f9ea
hiroto22 <tochihirototo.nya.49@gmail.com>
2022-09-24 21:48:03 +0900

    frourio tutorial introduce Aspida まで

---
+++ frourio-tutorial/aspida.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    input: "server/api",
+    baseURL: 'http://localhost:8888',
+  };
---
+++ frourio-tutorial/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import api from "./apiClient";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [greeting, setGreeting] = useState("loading...");
+
+  useEffect(() => {
+    api.$get().then((res) => {
+      setGreeting(res.hello);
+    });
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>{greeting}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
